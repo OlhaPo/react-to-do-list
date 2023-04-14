c2fb03077df217cf7e54e383427a89022884ab11
Olya <ponomaryova.olya.y@gmail.com>
2023-04-14 15:03:42 +0200

    Implement saving tasks to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import TaskForm from "./TaskForm";
 import Task from "./Task";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks || []);
+  }, []);
+
   function addTask(name) {
     setTasks((prev) => {
       return [...prev, { name: name, done: false }];
